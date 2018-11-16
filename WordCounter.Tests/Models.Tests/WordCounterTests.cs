ee55dc4479b6b1970c92540e21d5e060a8831f15
Gulzat Karimova <gulzat.karimova@gmail.com>
2018-11-16 14:53:28 -0800

    It will updated the file to complate tests for code.

--- WordCounter.Tests/Models.Tests/WordCounterTests.cs
+++ WordCounter.Tests/Models.Tests/WordCounterTests.cs
@@ -60,7 +60,31 @@ namespace WordCounter.Models.Tests
       string result = newRepeatCounter.GetSentence();
       // Assert
       Assert.AreEqual(newSentence, result);
-
     }
+    [TestMethod]
+    public void SplitSentence_ReturnWords_String()
+    {
+      // Arrange
+      string sentence = "I like my cat";
+      RepeatCounter newRepeatCounter = new RepeatCounter("", sentence);
+      // Act
+      string[] newSentence = {"I", "like", "my", "cat"};
+    string [] result = newRepeatCounter.SplitSentence();
+      // Assert
+      CollectionAssert.AreEqual(newSentence, result);
+    }
+    [TestMethod]
+    public void CountWords_CountWordsFromSentence_Int()
+    {
+    // Arrange
+    int result = 1;
+    string word = "cat";
+    string sentence = "I like my cat";
+    RepeatCounter newRepeatCounter = new RepeatCounter(word, sentence);
+    // Act
+    int newResult = newRepeatCounter.CountWords();
+    // Assert
+    Assert.AreEqual(result, newResult);
+  }
   }
 }
